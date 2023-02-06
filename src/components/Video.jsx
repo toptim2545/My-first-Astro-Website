1528d3ac749128b2cbd92d4d18ca6431ac59b3b1
toptim2545 <toptim2545@gmail.com>
2023-02-06 18:22:29 +0300

    chage 1st video

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -12,7 +12,7 @@ export const ReactExample = function ReactExample() {
         <iframe
           width="560"
           height="315"
-          src="https://www.youtube-nocookie.com/embed/SN3zbBUgSw0"
+          src="https://www.youtube-nocookie.com/embed/SbMBZB-R-8E"
           title="Актуальные новости киноиндустрии"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
