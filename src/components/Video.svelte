553bdd82b43c4f8929900605625ab4ba31883841
Andre Roussakoff <madrus@gmail.com>
2023-02-27 06:19:04 +0100

    add --color and --background vars

--- src/components/Video.svelte
+++ src/components/Video.svelte
@@ -1,58 +1,82 @@
 <script lang="ts">
-  let primary = "lightText";
-  let secondary = "darkText";
-  let hue = {
-    // lightText: "var(--colour-brand-hue)",
-    lightText: 39,
-    darkText: "var(--colour-dark-text-hue)",
-  };
-  let saturation = {
-    // lightText: "var(--colour-brand-saturation)",
-    lightText: "92%",
-    darkText: "var(--colour-dark-text-saturation)",
-  };
-  let luminance = {
-    // lightText: "var(--colour-brand-luminance)",
-    lightText: "57%",
-    darkText: "var(--colour-dark-text-luminance)",
-  };
+  export let altColours = false
+  export let hash: string = ''
+  export let title: string = ''
 
-  export let altColours = false;
-  export let hash: string = "";
-  export let title: string = "";
+  let fore = 'brand'
+  let back = 'darkText'
 
-  const youtube = "https://www.youtube-nocookie.com/embed/";
+  type HslProp = {
+    [key: string]: string
+  }
+
+  const hue: HslProp = {
+    brand: 'var(--colour-brand-hue)',
+    lightText: 'var(--colour-light-text-hue)',
+    darkText: 'var(--colour-dark-text-hue)',
+  }
+  const saturation: HslProp = {
+    brand: 'var(--colour-brand-saturation)',
+    lightText: 'var(--colour-light-text-saturation)',
+    darkText: 'var(--colour-dark-text-saturation)',
+  }
+  const luminance: HslProp = {
+    brand: 'var(--colour-brand-luminance)',
+    lightText: 'var(--colour-light-text-luminance)',
+    darkText: 'var(--colour-dark-text-luminance)',
+  }
+
+  let rootElement: HTMLElement
+  $: rootElement &&
+    rootElement.style.setProperty(
+      '--color',
+      `hsl(${hue[fore]}, ${saturation[fore]}, ${luminance[fore]})`
+    )
+  $: rootElement &&
+    rootElement.style.setProperty(
+      '--background',
+      `hsl(${hue[back]}, ${saturation[back]}, ${luminance[back]})`
+    )
+
+  const youtube = 'https://www.youtube-nocookie.com/embed/'
 </script>
 
-<section class={`container${altColours ? " container-alt" : ""}`}>
+<section
+  class="{`container${altColours ? ' container-alt' : ''}`}"
+  bind:this="{rootElement}"
+>
   <h2>{title}</h2>
   <div class="video-container">
     <iframe
-      {title}
+      title="{title}"
       width="560"
       height="315"
-      src={youtube + hash}
+      src="{youtube + hash}"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-    />
+      allowfullscreen></iframe>
   </div>
   <button
-    class={`button${altColours ? " button-alt" : ""}`}
-    on:click={() => {
-      altColours = !altColours;
-    }}><span class="screen-reader-text">Toggle colours</span></button
+    class="{`button${altColours ? ' button-alt' : ''}`}"
+    on:click="{() => {
+      altColours = !altColours
+    }}"><span class="screen-reader-text">Toggle colours</span></button
   >
 </section>
 
 <style>
+  :root {
+    --background: inherit;
+    --color: inherit;
+  }
+
   .container {
     display: flex;
     flex-direction: column;
-    background: hsl(hue[primary] saturation[primary] luminance[primary]);
+    background: var(--background);
+    color: var(--color);
     align-items: center;
     width: 100%;
     padding: var(--spacing-8) var(--spacing-0);
-    color: hsl(hue[secondary] saturation[secondary] luminance[secondary]);
   }
 </style>
